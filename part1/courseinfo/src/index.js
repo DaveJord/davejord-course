55263caf901378b708d32f27cc4cf374b84029bf
DaveJord <davidjordan@outlook.com>
2020-09-25 17:22:08 +0100

    this is a commit

---
+++ part1/courseinfo/src/index.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  const course = {
+  name: 'Half Stack application development',
+  parts: [
+  {
+    name: 'Fundamentals of React',
+    exercises: 10
+  },
+  {
+    name: 'Using props to pass data',
+    exercises: 7
+  },
+  {
+    name: 'State of a component',
+    exercises: 14
+  }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course.name}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  const [part1, part2, part3] = props.parts
+  return (
+    <div>
+      <Part part={part1.name} exercise={part1.exercises} />
+      <Part part={part2.name} exercise={part2.exercises} />
+      <Part part={part3.name} exercise={part3.exercises} />
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return(
+    <div>
+      <p>{props.part} {props.exercise}</p>
+    </div>
+  )
+}
+
+
+const Total = (props) => {
+  const [parts1, parts2, parts3] = props.parts
+    return(
+      <div>
+        <p>Number of exercises {parts1.exercises + parts2.exercises + parts3.exercises}</p>
+      </div>
+  )
+}
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
